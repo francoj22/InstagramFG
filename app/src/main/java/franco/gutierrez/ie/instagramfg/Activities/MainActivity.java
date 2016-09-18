c14f8c842a46db34aa43eaca9c21e598b916faa8
francoj22 <cl_mate@hotmail.com>
2016-09-18 22:00:18 +0100

    Milestone 1: adding sdk instagram

---
+++ app/src/main/java/franco/gutierrez/ie/instagramfg/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package franco.gutierrez.ie.instagramfg.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import franco.gutierrez.ie.instagramfg.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/franco/gutierrez/ie/instagramfg/Util/ApplicationData.java
@@ -0,0 +1,10 @@
+package franco.gutierrez.ie.instagramfg.Util;
+
+/**
+ * Created by Franco on 9/18/2016.
+ */
+public class ApplicationData {
+    public static final String Client_ID = "4577119038ca40e4b15bfacf1bbd68a2";
+    public static final String Client_Secret_ID = "2612ef332ebc4ee0828ef808fa285072";
+    public static final String CallBackURL = "http://showoff.ie/";
+}
